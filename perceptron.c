92f4cc111f8a446e76d86fc8e357b61c177f8191
Aisha Hadjou <aishaouladhadjou@gmail.com>
2025-01-04 21:35:22 +0100

    Added activation function and net input calculation for perceptron logic

--- perceptron.c
+++ perceptron.c
@@ -1,14 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+//Number of input features (Weather, Resources)
+#define NUM_INPUTS 2
+//Number of data samples   
+#define NUM_SAMPLES 100 
+typedef struct {
+    double weights[NUM_INPUTS];
+    double bias;
+} Perceptron;
+
 //Activation function (step function)
 int step_function(double net_input) {
     return (net_input >= 0) ? 1 : 0;
-}
-
-//Calculate the net input (weighted sum of inputs)
-double net_input(const Perceptron *perceptron, const double *inputs) {
-    double sum = 0.0;
-    for (int i = 0; i < NUM_INPUTS; ++i) {
-        sum += perceptron->weights[i] * inputs[i];
-    }
-    sum += perceptron->bias;
-    return sum;
 }
